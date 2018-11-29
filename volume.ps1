2fd995238986e6e5999e3d77a6f0babc143e592b
Gamer4life <uyenwontanswer@gmail.com>
2018-11-28 16:18:14 -0800

    Delete volume.ps1

--- volume.ps1
+++
@@ -1,5 +0,0 @@
-Param (
-    [string]$Volume 
-)
-Function Set-Speaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..$Volume | % {$wshShell.SendKeys([char]175)}}
-Set-Speaker -Volume $Volume
